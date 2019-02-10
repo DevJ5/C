5cf3de0bdcc0f0f18a23f598b11343d9d1e74925
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-02-10 17:28:12 +0100

    Create Swap.c

---
+++ Swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+    swap(&x, &y);
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
