2d3e296a537df3674915e40fe48f9ff9f9849fc7
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-02-19 06:53:37 +0100

    Arrays

---
+++ Arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 	Pointer to an array
+// You can generate a pointer to the first element of an array by simply specifying the array name, without any index.
+
+int main(void)
+{
+    // Declare the array:
+    int numbers[3];
+    numbers[0] = 4;
+    // Set the size of the array initially:
+    int number2[3] = {4, 5, 6};
+    // Omit the size:
+    int numbers3[] = {4, 5, 6};
+}
